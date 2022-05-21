ccefe218ad74c0d8e4ffa8054778c5b1610d2799
Gerson26 <gvelassco@gmail.com>
2022-05-20 20:58:46 -0500

    cambio de url transmision

--- backend/App/controllers/TransmissionVideo.php
+++ backend/App/controllers/TransmissionVideo.php
@@ -131,10 +131,10 @@ html;
        
 
 
-        $transmision_1 = TransmisionDao::getTransmisionById(4);
+        $transmision_1 = TransmisionDao::getTransmisionById(5);
         $transmision_2 = TransmisionDao::getTransmisionById(2);
 
-        $transmision_1_p1 = TransmisionDao::getTransmisionById(4);
+        $transmision_1_p1 = TransmisionDao::getTransmisionById(5);
         $transmision_2_p2 = TransmisionDao::getTransmisionById(2);
 
         $data_p1 = new \stdClass();
@@ -347,10 +347,10 @@ html;
        
 
 
-        $transmision_1 = TransmisionDao::getTransmisionById(1);
+        $transmision_1 = TransmisionDao::getTransmisionById(5);
         $transmision_2 = TransmisionDao::getTransmisionById(2);
 
-        $transmision_1_p1 = TransmisionDao::getTransmisionById(1);
+        $transmision_1_p1 = TransmisionDao::getTransmisionById(5);
         $transmision_2_p2 = TransmisionDao::getTransmisionById(2);
 
         $data_p1 = new \stdClass();
